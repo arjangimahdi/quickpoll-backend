afccb0a905179b10e6ff6e7e8672fd7c4a8f80c2
arjangimahdi <github.mahdiarjangi.official@gmail.com>
2026-06-18 18:14:39 +0330

    chore: update user schema

--- src/models/User.js
+++ src/models/User.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "Please add a password"],
-        minlength: 6,
+        minlength: 8,
         select: false,
     },
     refreshTokenHash: {
